088134a1e47e29e48c183e8a64ec579966b5a4ee
Luka <nikuradzeluka0@gmail.com>
2022-07-13 11:29:17 +0400

    commit

--- app/src/main/java/com/example/tbc_course_13/MainActivity.kt
+++ app/src/main/java/com/example/tbc_course_13/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.tbc_course_13
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tbc_course_13.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
 
         binding.bottomNavigation.selectedItemId = R.id.fragmentFifth
     }
